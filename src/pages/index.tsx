77737f512f029f45085a8b498251533528b15982
chronoseri <chronoseri@gmail.com>
2023-09-23 19:40:57 +0900

    ソース整理、図鑑番号検索追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,35 +4,33 @@ import { PokemonInfo } from "@/models/PokemonInfo";
 import "./index.css";
 import { useEffect, useState } from "react";
 import { NamedAPIResource, PokemonClient } from "pokenode-ts";
+import { IndexNumberSearch } from "@/components/IndexNumberSearch";
 
 export default function Home() {
   const [allPokemons, setAllPokemons] = useState<PokemonInfo[]>([]);
 
-  const getAllPokemons = async () => {
-    const api = new PokemonClient();
-    const pokemons = await api.listPokemons(0, 20);
-    pokemons.results.forEach((p) => {
-      console.log(p.name);
-    });
-    await createPokemonObject(pokemons.results);
+  const onClickSearch = async (val: number) => {
+    setAllPokemons([]);
+    await fetchTwentyPokemons(val);
   };
 
-  const createPokemonObject = async (results: NamedAPIResource[]) => {
+  const fetchTwentyPokemons = async (offset: number = 1) => {
     const api = new PokemonClient();
-    for (const res of results) {
-      const pokemon = await api.getPokemonByName(res.name);
+    for (let i = 0; i < 20; i++) {
+      const pokemon = await api.getPokemonById(i + offset);
       const newList: PokemonInfo = {
         id: pokemon.id,
         name: pokemon.name,
         image: pokemon.sprites.other!["official-artwork"].front_default!,
         type: pokemon.types[0].type.name,
       };
+
       setAllPokemons((currentList) => [...currentList, newList]);
     }
   };
 
   useEffect(() => {
-    getAllPokemons();
+    fetchTwentyPokemons();
   }, []);
 
   const list = allPokemons.map((pokemon) => (
@@ -42,6 +40,7 @@ export default function Home() {
   return (
     <Layout>
       <div className="app-container">
+        <IndexNumberSearch onClickSearch={onClickSearch}></IndexNumberSearch>
         <div className="pokemon-container">
           <div className="all-container">{list}</div>
         </div>
